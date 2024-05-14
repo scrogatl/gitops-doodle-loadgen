a9c031e229be051654849146188836dc9bf9b5b8
Scott Rogers <scottrogers@MacBook-Air.local>
2024-05-14 18:31:58 -0400

    adding src

---
+++ loadgen/app.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import time
+from datetime import datetime
+import sys
+
+
+def loadgen():
+    front_end = os.environ.get('FRONTEND_HOST', "localhost")
+    sleep_time = os.environ.get('SLEEP_TIME', "1")
+    print("FRONTEND_HOST: " + front_end)
+    print("SLEEP_TIME: " + sleep_time)
+    # 
+    while (True):
+        timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+        try:
+            res = requests.get('http://' + front_end + ':5000', timeout=.5)
+            if res.status_code >= 300: 
+                print(timeString + " - [loadgen] - Status: " + str(res.status_code) + " - " + res.text )
+            else: 
+                print(timeString + " - [loadgen] - Status: " + str(res.status_code) + " - " + res.text)
+        except Exception as e:
+            print(timeString + " - [loadgen] - Status: " + repr(e))
+        time.sleep( int(sleep_time) / 1000 )
+
+if __name__ == '__main__':
+    sys.exit(loadgen())  
+
+

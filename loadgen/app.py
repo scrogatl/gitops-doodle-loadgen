42c5b8b853a62d15eb7116c9eb3eab35b2c060e1
Scott Rogers <scott@scottandrewrogers.com>
2024-05-15 12:45:11 -0400

    small change

--- loadgen/app.py
+++ loadgen/app.py
@@ -7,7 +7,7 @@ import sys
 
 def loadgen():
     front_end = os.environ.get('FRONTEND_HOST', "localhost")
-    sleep_time = os.environ.get('SLEEP_TIME', "2")
+    sleep_time = os.environ.get('SLEEP_TIME', "3")
     print("FRONTEND_HOST: " + front_end)
     print("SLEEP_TIME: " + sleep_time)
     

22729dda73b9c96a89dc416c0d61023b1bd0ab22
scrogatl <scott@scottandrewrogers.com>
2025-11-16 00:44:44 -0500

    detect az container env

--- loadgen/app.py
+++ loadgen/app.py
@@ -6,14 +6,20 @@ import sys
 
 
 def loadgen():
-    front_end = os.environ.get('F_HOST', "localhost")
+    front_end      = os.environ.get('F_HOST', "localhost")
     front_end_port = os.environ.get('F_PORT', "5000")
-    sleep_time = os.environ.get('SLEEP_TIME', "5000")
+    sleep_time     = os.environ.get('SLEEP_TIME', "5000")
+    az_dns_suffix  = os.environ.get('CONTAINER_APP_ENV_DNS_SUFFIX')
     print("F_HOST: " + front_end)
     print("F_PORT: " + front_end_port)
     print("SLEEP_TIME: " + sleep_time)
+
+    rq_url = ""
+    if az_dns_suffix:
+        rq_url = 'https://' + front_end + '.' + az_dns_suffix
+    else:
+        rq_url = 'http://' + front_end + ':' + front_end_port
     
-    rq_url = 'http://' + front_end + ':' + front_end_port
     while (True):
         timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
         try:
